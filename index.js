a04e3657ca55db49b3ad401a99fe34d5db280277
Huzaifa-Munir-Mavariya <Huzaifa1231234564567@gmail.com>
2023-11-08 07:30:29 -0800

    changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const connectToMongo = require("./db");
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 
